2a6a3c90b0b231bfd8327cdedc1917d2941e1f79
Margaret <margaret89ru@gmail.com>
2023-03-12 22:26:57 +0500

    Переделка с библиотеки YaDisk на requests

--- YaDisk.py
+++ YaDisk.py
@@ -1,14 +1,24 @@
 import os
-import yadisk
+import requests
 
 
 class YaUploader:
     def __init__(self, token: str):
-        self.token = token
+        self.Token = token
+        self.Url = "https://cloud-api.yandex.net/v1/disk/resources1"
+        self.Headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {self.Token}'}
 
     def upload(self, file_path: str):
-        yd = yadisk.YaDisk(token=self.token)
-        yd.upload(file_path, os.path.basename(file_path))
+        res = requests.get(self.Url, headers=self.Headers)
+        if res.status_code != 400:
+            return f"Error: {res.text}"
+
+        res = requests.get(f'{self.Url}/upload?path={os.path.basename(file_path)}&overwrite=True', headers=self.Headers).json()
+        if 'href' not in res.keys():
+            return f"Error: {res['error']}"
+        else:
+            with open(file_path, 'rb') as f:
+                requests.put(res['href'], files={'file': f})
         return f"File {file_path} uploaded"
 
 
